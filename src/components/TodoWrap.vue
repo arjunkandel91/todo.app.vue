78c2c425587a64b823fbdfdb7524d01bd4f8b027
Arjun Kandel <arjun.kandel@live.com>
2026-02-10 14:23:55 +0545

    few updates related to styling

--- src/components/TodoWrap.vue
+++ src/components/TodoWrap.vue
@@ -2,4 +2,9 @@
     <div class="todo-app">
         <slot></slot>
     </div>
+
+    <div class="info">
+        <a href="https://github.com/arjunkandel91/todo.app.vue" target="_blank">This project was built using Vue.js</a>
+        <a href="#!" id="ClearStorage">Clear Storage</a>
+    </div>
 </template>
